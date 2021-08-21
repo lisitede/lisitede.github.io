3aec0d7b7bd48cf71b101fb5a4b9b29f36660389
hbrls <shuaizhexu@gmail.com>
2021-08-21 21:40:41 +0800

    A: vuepress init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+const navbar = require('./navbar');
+const sidebar = require('./sidebar');
+
+
+module.exports = {
+  host: '0.0.0.0',
+  port: '3001',
+
+  lang: 'zh-CN',
+  title: '理斯特的',
+  description: '技能 / 知识 / 经验',
+
+  themeConfig: {
+    lastUpdated: false,
+    contributors: false,
+
+    logo: 'https://cdn.lisitede.com/logo-lisitede.png',
+
+    navbar,
+    sidebar,
+  },
+
+  plugins: [
+    [ '@vuepress/plugin-google-analytics', { id: 'G-96EPGBWZFV' } ],
+  ],
+};
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+理斯特的
+==
+
+技能 / 知识 / 经验
+--
+
+https://lisitede.com
+
+
+<a href="http://www.beian.miit.gov.cn" target="_blank">沪ICP备15047919号</a>
