aaf88c8306b2e88c6525384a03c3d5e07896dc45
hbrls <shuaizhexu@gmail.com>
2024-03-31 15:07:05 +0800

    R: vitepress@1.0.0

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,34 +1,36 @@
-module.exports = [
+const nav = [
   {
-    text: '首页',
-    link: '/',
+    text: "首页",
+    link: "/",
   },
   {
-    text: '小鲜',
+    text: "小鲜",
     items: [
-      { text: '职业', link: '/pages/awesome/career/caocao/' },
-      { text: '研报', link: '/pages/awesome/research-report/' },
+      { text: "职业", link: "/pages/awesome/career/caocao/" },
+      { text: "研报", link: "/pages/awesome/research-report/" },
     ],
   },
   {
-    text: '微信非官方文档',
+    text: "微信非官方文档",
     items: [
-      { text: '25 位银行账户', link: '/pages/wechat/25-yinhang-zhanghao/' },
+      { text: "25 位银行账户", link: "/pages/wechat/25-yinhang-zhanghao/" },
     ],
   },
   {
-    text: '假文',
+    text: "假文",
     items: [
-      { text: '微博', link: '/pages/lorem/weibo/' },
-      { text: '小丧怪', link: '/pages/lorem/xiaosangguai/' },
-      { text: '对齐', link: '/pages/lorem/align/' },
+      { text: "微博", link: "/pages/lorem/weibo/" },
+      { text: "小丧怪", link: "/pages/lorem/xiaosangguai/" },
+      { text: "对齐", link: "/pages/lorem/align/" },
     ],
   },
   {
-    text: '阅读',
+    text: "阅读",
     items: [
-      { text: '设计', link: '/pages/read/design/' },
-      { text: '领域驱动设计', link: '/pages/read/ddd/' },
+      { text: "设计", link: "/pages/read/design/" },
+      { text: "领域驱动设计", link: "/pages/read/ddd/" },
     ],
-  }
+  },
 ];
+
+export default nav;
